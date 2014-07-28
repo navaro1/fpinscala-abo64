7c4564423c10bbed314e23342b4a3bdc089b8e9d
abo64 <abo64@gmx.de>
2014-07-28 21:46:42 +0200

    solutions and tests for chapter 6 RNG

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -82,7 +82,23 @@ object RNG {
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
     fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, rng2) = f(rng)
+      g(a)(rng2)
+    }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n-1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
+
+  def mapViaFlatMap[A,B](ra: Rand[A])(f: A => B): Rand[B] =
+    flatMap(ra)(a => unit(f(a)))
+
+  def map2ViaFlatMap[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
 }
 
 case class State[S,+A](run: S => (A, S)) {
--- exercises/src/test/scala/fpinscala/state/StateSpec.scala
+++ exercises/src/test/scala/fpinscala/state/StateSpec.scala
@@ -44,7 +44,7 @@ class StateSpec extends FlatSpec with PropertyChecks with Matchers {
 
   behavior of "6.2 double"
 
-  def testDoubleRange(d: Double) = d should (be >= 0d and be < 1d)
+  def testDoubleRange(d: Double, offset: Int = 0) = d should (be >= 0d + offset and be < 1d + offset)
 
   def testDouble(n: Int) = testDoubleRange(double(TestRNG(n))._1)
 
@@ -129,7 +129,7 @@ class StateSpec extends FlatSpec with PropertyChecks with Matchers {
   }
 
   it should "return a value >= 0 and < 1 for all nextInt values" in {
-    testCornerCases(testDoubleViaMap)
+    testProperty(testDoubleViaMap)
   }
 
   behavior of "6.6 map2"
@@ -144,7 +144,7 @@ class StateSpec extends FlatSpec with PropertyChecks with Matchers {
   }
 
   it should "return a value >= 0 and < 1 for all nextInt values" in {
-    testCornerCases(testMap2)
+    testProperty(testMap2)
   }
 
   behavior of "6.7 sequence"
@@ -165,6 +165,66 @@ class StateSpec extends FlatSpec with PropertyChecks with Matchers {
     forAll(Gen.chooseNum(0, 1) :| "count: Int") { count: Int =>
       testProperty(testSequence(count))
     }
+  }
+
+  behavior of "6.8.1 flatMap"
+
+  def testFlatMap(n: Int) = {
+    val (d,_) = flatMap(double)(rd => rng => (42 + rd, rng))(TestRNG(n))
+    testDoubleRange(d, 42)
+  }
+
+  it should "work for corner cases" in {
+    testCornerCases(testFlatMap)
+  }
+
+  it should "return a value >= 42.0 and < 43.0 for all nextInt values" in {
+    testProperty(testFlatMap)
+  }
+
+  behavior of "6.8.2 nonNegativeLessThan"
+
+  def testNonNegativeLessThan(n: Int) = {
+    val (i,_) = nonNegativeLessThan(42)(TestRNG(n))
+    i should (be >= 0 and be < 42)
+  }
+
+  it should "work for corner cases" in {
+    testCornerCases(testNonNegativeLessThan)
+  }
+
+  it should "return a value >= 0 and < 42 for all nextInt values" in {
+    testProperty(testNonNegativeLessThan)
+  }
+
+  behavior of "6.9.1 mapViaFlatMap"
+
+  def testMapViaFlatMap(n: Int) = {
+    val (d, _) = mapViaFlatMap(double)(_ + 42)(TestRNG(n))
+    testDoubleRange(d, 42)
+  }
+
+  it should "work for corner cases" in {
+    testCornerCases(testMapViaFlatMap)
+  }
+
+  it should "return a value >= 0 and < 1 for all nextInt values" in {
+    testProperty(testMapViaFlatMap)
+  }
+
+  behavior of "6.9.2 map2ViaFlatMap"
+
+  def testMap2ViaFlatMap(n: Int) = {
+    val (d, _) = map2ViaFlatMap(double, double)(_ * _)(TestRNG(n))
+    testDoubleRange(d)
+  }
+
+  it should "work for corner cases" in {
+    testCornerCases(testMap2ViaFlatMap)
+  }
+
+  it should "return a value >= 0 and < 1 for all nextInt values" in {
+    testProperty(testMap2ViaFlatMap)
   }
 
 }
