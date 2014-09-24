64dc2f649d6c6cb1e33b1093e85738030e8ab289
abo64 <abo64@gmx.de>
2014-09-24 07:59:32 +0200

    GoogleBillboardPuzzle

--- exercises/src/main/scala/fpinscala/laziness/GoogleBillboardPuzzle.scala
+++ exercises/src/main/scala/fpinscala/laziness/GoogleBillboardPuzzle.scala
@@ -1,12 +1,16 @@
 package fpinscala.laziness
 
 import java.math.MathContext
-import scala.concurrent.duration.Duration
 import java.util.concurrent.TimeUnit
+
+import scala.concurrent.duration.Duration
+import scala.math.BigDecimal.int2bigDecimal
 
 object GoogleBillboardPuzzle extends App {
 
   // Kestrel combinator: perform some side effect before returning result
+  // http://stackoverflow.com/questions/9671620/how-to-keep-return-value-when-logging-in-scala/9673294#9673294
+  // http://debasishg.blogspot.de/2009/09/side-effects-with-kestrel-in-scala.html
   private def kestrel[A](x: A)(f: A => Unit): A = { f(x); x }
 
   private def timed[A](name: String)(block: => A): A = {
@@ -17,64 +21,74 @@ object GoogleBillboardPuzzle extends App {
     }
   }
 
-  // # of iterations required to get good enough precision of Euler's number
-  val EulerIterations = 75
+  object EulerCalculator {
+    // # of iterations required to get good enough precision of Euler's number
+    val EulerIterations = 75
 
-  val DecimalPrecision = new MathContext(10000)
-  val Zero = BigDecimal(0, DecimalPrecision)
-  val One = BigDecimal(1, DecimalPrecision)
+    val DecimalPrecision = new MathContext(10000)
+    val Zero = BigDecimal(0, DecimalPrecision)
+    val One = BigDecimal(1, DecimalPrecision)
 
-  def inverse(n: BigDecimal): BigDecimal = //kestrel(One / n)(_ => println(n))
-    One / n
+    def inverse(n: BigDecimal): BigDecimal = //kestrel(One / n)(_ => println(n))
+      One / n
 
-  def factorial(n: Int): BigDecimal =
-    if (n == 0) One else (1 to n).foldLeft(One)(_ * _)
+    def factorial(n: Int): BigDecimal =
+      if (n == 0) One else (1 to n).foldLeft(One)(_ * _)
 
-  def addEulerSeriesElement(b: BigDecimal, i: Int): BigDecimal =
-    b + inverse(factorial(i))
+    def addEulerSeriesElement(b: BigDecimal, i: Int): BigDecimal =
+      b + inverse(factorial(i))
 
-  def calculateEuler(n: Int): BigDecimal = {
-    (0 to n).toSeq.foldLeft(Zero)(addEulerSeriesElement)
-  }
-
-  /** Stream of BigDecimals approaching Euler's number */
-  def eulerStream: Stream[BigDecimal] =
-    Stream.unfold((0, Zero)) {
-      case (n, e) => {
-        val euler = addEulerSeriesElement(e, n)
-        Some((euler, (n + 1, euler)))
-      }
+    def calculateEuler(n: Int): BigDecimal = {
+      (0 to n).toSeq.foldLeft(Zero)(addEulerSeriesElement)
     }
 
-  def isPrime(i: Long) : Boolean = {
-    if (i <= 1) false
-    else if (i == 2) true
-    else if (i % 2 == 0) false
-    else !(3L to math.sqrt(i).toLong by 2).exists(i % _ == 0)
-  }
-  //(0 to 100) foreach { x => println(s"$x: ${isPrime(x)}")}
+    /** Stream of BigDecimals approaching Euler's number */
+    def eulerStream: Stream[BigDecimal] =
+      Stream.unfold((0, Zero)) {
+        case (n, e) => {
+          val euler = addEulerSeriesElement(e, n)
+          Some((euler, (n + 1, euler)))
+        }
+      }
 
-  def findFirstPrime(str: String, digits: Int): Option[String] = {
-    str.sliding(digits).find(s =>
-      s.length == digits && //kestrel(isPrime(s.toLong))(b => if (b) println(s)))
-      isPrime(s.toLong))
-  }
-
-  def decimalsAsString(n: BigDecimal) = {
-    val str = n.toString
-    val decPos = str.indexWhere(_ == '.')
-    if (decPos > -1) str.substring(decPos + 1) else ""
+    // make sure that decimal precision is high enough
+    def preciseEulerStream = eulerStream.drop(EulerIterations)
   }
 
-  def findStreamSolution(digits: Int) = {
-    var n = 0
-    def moreIterations = kestrel(n < EulerIterations)(_ => n += 1) // make sure that precision is high enough
-    val solutionStream = eulerStream map decimalsAsString map(findFirstPrime(_, digits))
-    solutionStream.dropWhile(moreIterations || _.isEmpty).headOption.get
+  object PrimeFinder {
+    def isPrime(i: Long): Boolean = {
+      if (i <= 1) false
+      else if (i == 2) true
+      else if (i % 2 == 0) false
+      else !(3L to math.sqrt(i).toLong by 2).exists(i % _ == 0)
+    }
+    //(0 to 100) foreach { x => println(s"$x: ${isPrime(x)}")}
+
+    def findFirstPrime(str: String, digits: Int): Option[String] = {
+      str.sliding(digits).find(s =>
+        s.length == digits && //kestrel(isPrime(s.toLong))(b => if (b) println(s)))
+        isPrime(s.toLong))
+    }
+
+    def findStreamSolution(primeDigits: Int): Option[String] = {
+      import EulerCalculator._
+      val solutionStream: Stream[Option[String]] =
+        preciseEulerStream map decimalsAsString map (findFirstPrime(_, primeDigits))
+      solutionStream.headOption.get
+    }
+
+    def decimalsAsString(n: BigDecimal): String = {
+      val str = n.toString
+      val decPos = str.indexWhere(_ == '.')
+      if (decPos > -1) str.substring(decPos + 1) else ""
+    }
   }
 
-  timed("Euler w/ loop  ")(calculateEuler(EulerIterations))
-  timed("Euler w/ Stream")(eulerStream.drop(EulerIterations).headOption.get)
+  import EulerCalculator._
+  import PrimeFinder._
+
+  timed("  Euler w/ fold")(calculateEuler(EulerIterations))
+  timed("Euler w/ unfold")(preciseEulerStream.headOption.get)
 
   val ExpectedSolution = 7427466391L
   def checkSolution(solution: Option[String]): Unit =
@@ -85,7 +99,7 @@ object GoogleBillboardPuzzle extends App {
   }
   checkSolution(solution)
 
-  val streamSolution = timed("solution w/ mapped Stream "){
+  val streamSolution = timed(" solution w/ mapped Stream"){
     findStreamSolution(10)
   }
   checkSolution(streamSolution)
