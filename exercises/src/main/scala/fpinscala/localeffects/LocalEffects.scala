c5ced08603c8d4e6a32d3ba2319c89403f167418
abo64 <abo64@gmx.de>
2014-11-04 20:42:40 +0100

    14.2.1 STArray.partition

--- exercises/src/main/scala/fpinscala/localeffects/LocalEffects.scala
+++ exercises/src/main/scala/fpinscala/localeffects/LocalEffects.scala
@@ -133,9 +133,28 @@ object STArray {
 object Immutable {
   def noop[S] = ST[S,Unit](())
 
-  def partition[S](a: STArray[S,Int], l: Int, r: Int, pivot: Int): ST[S,Int] = ???
+  def partition[S](a: STArray[S,Int], l: Int, r: Int, pivot: Int): ST[S,Int] = for {
+    vp <- a.read(pivot)
+    _ <- a.swap(pivot, r)
+    j <- STRef(l)
+    _ <- (l until r).foldLeft(noop[S])((s, i) => for {
+      _ <- s
+      vi <- a.read(i)
+      _  <- if (vi < vp) (for {
+        vj <- j.read
+        _  <- a.swap(i, vj)
+        _  <- j.write(vj + 1)
+      } yield ()) else noop[S]
+    } yield ())
+    x <- j.read
+    _ <- a.swap(x, r)
+  } yield x
 
-  def qs[S](a: STArray[S,Int], l: Int, r: Int): ST[S, Unit] = ???
+  def qs[S](a: STArray[S,Int], l: Int, r: Int): ST[S, Unit] =  if (l < r) for {
+    pi <- partition(a, l, r, l + (r - l) / 2)
+    _ <- qs(a, l, pi - 1)
+    _ <- qs(a, pi + 1, r)
+  } yield () else noop[S]
 
   def quicksort(xs: List[Int]): List[Int] =
     if (xs.isEmpty) xs else ST.runST(new RunnableST[List[Int]] {
--- exercises/src/test/scala/fpinscala/localeffects/LocalEffectsSpec.scala
+++ exercises/src/test/scala/fpinscala/localeffects/LocalEffectsSpec.scala
@@ -3,6 +3,7 @@ package fpinscala.localeffects
 import org.junit.runner.RunWith
 import org.scalatest.FlatSpec
 import org.scalatest.prop.PropertyChecks
+import org.scalacheck.Gen
 
 @RunWith(classOf[org.scalatest.junit.JUnitRunner])
 class LocalEffectsSpec extends FlatSpec with PropertyChecks {
@@ -20,6 +21,32 @@ class LocalEffectsSpec extends FlatSpec with PropertyChecks {
       }
       val result = ST.runST(runnableST)
       assert(result == ints)
+    }
+  }
+
+  behavior of "14.2.1 STArray.partition"
+  it should "work" in {
+    val ints = Gen.choose(-10, 10)
+    val intList = Gen.choose(1, 10).flatMap(Gen.listOfN(_, ints)).map(_.toSet).map(_.toList)
+    val pivot = 0
+    forAll(intList label "ints") { ints: List[Int] =>
+      whenever(!ints.isEmpty) {
+        val l = 0
+        val r = ints.size - 1
+        val pivot = l + (r - l) / 2
+        val pivotValue = ints(pivot)
+        val partitions = ints.partition(_ <= pivotValue)
+        val runnableST = new RunnableST[(List[Int], Int)] {
+          override def apply[S] = for {
+            array <- STArray.fromList[S, Int](ints)
+            p <- Immutable.partition(array, l, r, pivot)
+            list <- array.freeze
+          } yield (list, p)
+        }
+        val result = ST.runST(runnableST)
+        val resultPartition = result._1.splitAt(result._2 + 1)._1
+        assert(resultPartition.toSet == partitions._1.toSet)
+      }
     }
   }
 }
