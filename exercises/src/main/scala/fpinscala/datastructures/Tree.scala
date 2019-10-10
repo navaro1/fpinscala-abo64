b910a58ac7839e58a17a1a94243414684b02cedc
Piotr Lewandowski <piotr@petlew.com>
2019-10-10 19:21:53 +0200

    Exercises until 3.29

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -14,18 +14,33 @@ object Tree {
     case Branch(left, right) => 1 + size(left) + size(right)
   }
 
-  def maximum(t: Tree[Int]): Int = sys.error("todo")
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(x) => x
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
 
-  def depth(t: Tree[_]): Int = sys.error("todo")
+  def depth(t: Tree[_]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + depth(l).max(depth(r))
+  }
 
-  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = sys.error("todo")
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(x) => Leaf(f(x))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
 
-  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = sys.error("todo")
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(x) => f(x)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
 
   def sizeViaFold[A](t: Tree[A]): Int =
     fold(t)(a => 1)(1 + _ + _)
 
-  def maximumViaFold[T](t: Tree[T])(implicit ev: Numeric[T]): T = sys.error("todo")
+  def maximumViaFold[T](t: Tree[T])(implicit ev: Numeric[T]): T =
+    fold(t)(x => x)(ev.max)
 
-  def depthViaFold[A](t: Tree[A]): Int = sys.error("todo")
+
+  def depthViaFold[A](t: Tree[A]): Int =
+    fold(t)(a => 0)((l, r) => 1 + l.max(r))
 }
